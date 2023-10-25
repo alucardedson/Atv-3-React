9fba1f6d9379ca59196704821c08aed8801dcdb5
alucardedson <alucardedson@gmail.com>
2023-10-24 21:44:36 -0300

    Atv-3-React
    Criação de componente

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Produto from './assets/components/Produto'
+
+function App() {
+
+  return (
+    <div>
+      
+      <Produto/>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ assets/components/Produto.jsx
@@ -0,0 +1,13 @@
+
+export default function Produto() {
+
+  return (
+    <div>
+      
+      <h1>Meu primeiro componente</h1>
+      
+      
+    </div>
+  )
+}
+
